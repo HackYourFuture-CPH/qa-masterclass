24e91c3b3c2a01d8862e31b2a7f40320d443e3f3
27ManishaPatel <manisha270989@gmail.com>
2023-10-25 21:56:21 +0200

    week4 testing

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -1,9 +1,18 @@
-import { Page } from "playwright/test";
-
-
-export class Login{
-    private page : Page;
-    constructor(page : Page){
-        this.page = page;
-    }
+import { Page, expect } from "@playwright/test";
+export class LoginPage {
+  private page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+  async visit() {
+    await this.page.goto("https://qa-challenge-hyf.planday.com");
+  }
+  async acceptCookies() {
+    await this.page.getByRole("button", { name: "OK" }).click();
+  }
+  async login() {
+    await this.page.getByPlaceholder('E.g. adam@planday.com').fill('gijanat705weirby.com');
+    await this.page.getByLabel('Password', { exact: true }).fill('HYF_QA123');
+    await this.page.getByLabel('Log in').click();
+  }
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login-page';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -15,4 +16,21 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+test("Login and verify that user lands on the home page", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.visit();
+  await loginPage.acceptCookies();
+  await loginPage.login();
+
+  //Assertion
+
+  await expect(page).toHaveURL(/.*home/);
+  await expect(page.getByRole("link", { name: "Home" })).toBeVisible();
+
+  
+  expect(page).toHaveURL("https://qa-challenge-hyf.planday.com")
+  await expect(page.getByRole("heading", { name: 'HACKYOURFUTURE' })).toBeVisible();
+  await expect(page.getByRole("link", { name: 'HOME' })).toBeVisible();
+
 });
